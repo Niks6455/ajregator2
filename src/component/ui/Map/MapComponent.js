2e2d489ed730f871e5f83e851229fa62d2a5ed67
ArturTonoyan <92105368+ArturTonoyan@users.noreply.github.com>
2023-04-15 13:43:17 +0300

    tmap1

--- src/component/ui/Map/MapComponent.js
+++ src/component/ui/Map/MapComponent.js
@@ -1,14 +1,15 @@
 import React from 'react';
 import { YMaps, Map } from 'react-yandex-maps';
-
+import "./mapComponent.scss"
 function MapComponent({w, h}) {
   return (
     <div style={{height: `${h +'px'}`,width: `${w +'px'}` }}>
       <YMaps>
         <Map
-          defaultState={{ center: [47.236726, 38.892407], zoom: 17}}
+          defaultState={{ center: [47.208208, 38.937189], zoom: 13}}
           width={'100%'}
           height={'100%'}
+          
           
         />
       </YMaps>
