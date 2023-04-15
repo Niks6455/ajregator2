6d7bd7a8d239bf4b059b42d747d2a296716ad1a3
ArturTonoyan <92105368+ArturTonoyan@users.noreply.github.com>
2023-04-15 18:16:35 +0300

    3232к

--- src/component/ui/Map/MapComponent.js
+++ src/component/ui/Map/MapComponent.js
@@ -1,28 +1,41 @@
 import React from 'react';
-import { YMaps, Map, Placemark } from 'react-yandex-maps';
+import { YMaps, Map, Placemark, GeolocationControl,RouteButton, SearchControl } from 'react-yandex-maps';
+import { TrafficControl, ZoomControl } from 'react-yandex-maps';
 // import "./mapComponent.scss"
 
 function MapComponent({w, h}) {
 
   return (
     <div style={{height: `${h +'px'}`,width: `${w +'px'}` }}>
-      <YMaps>
+      <YMaps query={{ apikey: 'f3c78576-996b-4eaa-84f8-12a8520d276a' }}>
           <Map width={'100%'} height={'100%'}
               defaultState={{
                 center: [47.208208, 38.937189],
                 zoom: 13,
-                controls: ["zoomControl", "fullscreenControl"],
+                
               }}
-              modules={["control.ZoomControl", "control.FullscreenControl"]}
             >
             <Placemark
               modules={["geoObject.addon.balloon"]}
-              defaultGeometry={[55.75, 37.57]}
+              defaultGeometry={[47.208208, 38.937189]}
               properties={{
                 balloonContentBody:
-                  "This is balloon loaded by the Yandex.Maps API module system",
+                  "Автомойка1",
               }}
             />
+             <Placemark
+              modules={["geoObject.addon.balloon"]}
+              defaultGeometry={[47.230820, 38.927189]}
+              properties={{
+                balloonContentBody:
+                  "Автомойка2",
+              }}
+            />
+            <GeolocationControl options={{ float: "left" }} />
+            <RouteButton options={{ float: "right" }} />
+            <SearchControl options={{ float: "right" }} />
+            <TrafficControl options={{ float: "right" }} />
+            <ZoomControl options={{ float: "right" }} />
       </Map>
       </YMaps>
     </div>
